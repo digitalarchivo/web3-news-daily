bb596da7a89795068ff87f36e7adf764beff9a4e
Benwebdev <hello@benline.co.uk>
2023-11-03 14:16:54 +0000

    leeroyyyyy

--- yarb-scrape.py
+++ yarb-scrape.py
@@ -1,10 +1,12 @@
 # coding:utf-8
-from my_custom_package import add_to_readme, todayDateFileName, strdate, createAndScrape
+from my_custom_package import add_to_readme, todayDateFileName, strdate, createAndScrape, loopscrape
 
 def job():
     createAndScrape('Web3 Daily News Feed', todayDateFileName, '2023', strdate, isToday=True)
     add_to_readme(todayDateFileName, strdate)
 
+    loopscrape()
+    
 
 
 if __name__ == '__main__':
