9c26e490980c07207bb522da8eed944fcef7ae77
Benwebdev <hello@benline.co.uk>
2023-11-03 12:00:37 +0000

    add

--- yarb-scrape.py
+++ yarb-scrape.py
@@ -164,13 +164,17 @@ def job():
     add_to_readme(todayDateFileName, strdate)
 
     # loop 5 times - 307
-    for i in range(1, 307):
+    for i in range(1, 424):
         # minus one day
         runDate2 = (datetime.datetime.now() - datetime.timedelta(days=i)).strftime('%Y-%m-%d')
+        yearString = runDate2.split('-')[0]
+        print(yearString)
         filename2 = 'days/{date}.md'.format(date=runDate2)
         # createAndScrape('Web3 Daily News Feed', filename, '2023', runDate)
         createAndScrape('Web3 Daily News Feed', filename2, '2023', runDate2)
         add_to_readme(filename2, runDate2)
+        # sleep 1 second
+        # time.sleep(1)
 
 if __name__ == '__main__':
     job()
