79cd33bf712820f845f9ed4c892dd53be1f74b9b
Benwebdev <hello@benline.co.uk>
2023-11-03 17:01:45 +0000

    🔧 chore(schedule.yml): enable push trigger for the Yarb Scrape workflow to run on main and releases branches 🗑️ chore(schedule.yml): remove commented out code for push trigger on specific branches 🗑️ chore(schedule.yml): remove unused files date.json, parse.py, test.py, and trans.py 🔄 chore(schedule.yml): update pythonmodules submodule commit reference to 19b84143838d39dc1c95db7cbc90cc0471991a72

--- test.py
+++
@@ -1,6 +0,0 @@
-from my_custom_package import package_function, write_to_top_of_file
-
-# write_to_top_of_file('test.md', 'This will be written to the top of the file.\n')
-print(package_function())
-
-write_to_top_of_file('README.md', 'This will be written to the top of the file.\n')
